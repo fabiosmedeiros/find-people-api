7bc2d31a300e5a27d6d282dc3bcfa357cadfd0ad
Fabio <fabiosmedeiros@gmail.com>
2022-02-11 08:53:22 -0300

    Developed unit tests for create person.

---
+++ src/test/java/com/fabiosmedeiros/personapi/service/PersonServiceTest.java
@@ -0,0 +1,49 @@
+package com.fabiosmedeiros.personapi.service;
+
+import com.fabiosmedeiros.personapi.dto.MessageResponseDTO;
+import com.fabiosmedeiros.personapi.dto.PersonDTO;
+import com.fabiosmedeiros.personapi.entity.Person;
+import com.fabiosmedeiros.personapi.repository.PersonRepository;
+import com.fabiosmedeiros.personapi.utils.PersonUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = PersonUtils.createFakeFTO();
+        Person expectedSavedPerson = PersonUtils.createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+
+}
---
+++ src/test/java/com/fabiosmedeiros/personapi/utils/PhoneUtils.java
@@ -0,0 +1,28 @@
+package com.fabiosmedeiros.personapi.utils;
+
+import com.fabiosmedeiros.personapi.dto.PhoneDTO;
+import com.fabiosmedeiros.personapi.entity.Phone;
+import com.fabiosmedeiros.personapi.enums.PhoneType;
+
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+}
